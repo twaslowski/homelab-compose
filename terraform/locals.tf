dda63ec4eb0617eb5cb6ece3c1998e40bcd7a813
Tobias Waslowski <tobias.waslowski@otto-payments.de>
2025-12-17 13:15:43 +0100

    fix: remove tandoor nginx proxy, expose service directly

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,6 +4,10 @@ locals {
       hostname = "memos.${var.domain}"
       service  = "http://memos:5230"
     }
+    recipes = {
+      hostname = "recipes.${var.domain}"
+      service  = "http://tandoor:8080"
+    }
     vaultwarden = {
       hostname = "passwords.${var.domain}"
       service  = "http://vaultwarden:8080"
